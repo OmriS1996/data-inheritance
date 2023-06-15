dbbdbeef251f33e7cd4fabd4398f580b93535bfc
OmriS1996 <os1996@walla.com>
2023-06-15 12:00:55 +0300

    bugs and readme update
    Updated the code to support only specific field in order to prevent bugs and crushes, and also fully and seamlessly support all fields.

--- README.md
+++ README.md
@@ -6,3 +6,11 @@ Current used files:
 
 The first file is the actual plugin logic, the second is a life cycle method to register that middleware to the main app.
 The rest of the files are simply part of Strapi's plugin template.
+
+Usage
+Just add to the plugins directory in your main application, also add the plugin in the plugins file, according to Strapi's documents.
+Make sure that the keys names are all unique.
+when making a GET request, it should get only the fields that you requested, like using Strapi normally, only that nulls are populated from a relation of the same entity if one exists.
+
+The plugin only populates all of the "normal" fields, components, relations and Dynamic Zones are not checked.
+The plugin iterates over all of the entity's relations, while remembering which relations it already checked.
--- server/middlewares/dataInherit.ts
+++ server/middlewares/dataInherit.ts
@@ -31,7 +31,6 @@ export default async (ctx: Context, next: Next) => {
   if (!ctx.body?.data) {
     return;
   }
-
   const collectionName = ctx?.request?.url?.split("/")[2];
 
   const collectionUID = getCollectionUID(collectionName);
@@ -46,7 +45,7 @@ export default async (ctx: Context, next: Next) => {
     return;
   }
 
-  const nullsNames = getNullsNames(ctx.body.data);
+  const nullsNames = getNullsNames(ctx.body.data, collectionUID);
 
   if (nullsNames.length == 0) {
     return;
@@ -88,14 +87,53 @@ function getRelationsNames(collectionUID: string): string[] {
   return relationsNames;
 }
 
-function getNullsNames(data: Data): string[] {
+function getNullsNames(data: Data, collectionUID: string): string[] {
   let nullsNames: string[] = [];
+  const schema: Schema = strapi.contentTypes[collectionUID].__schema__;
   for (const item in data.attributes) {
+    if (!schema.attributes.hasOwnProperty(item)) {
+      continue;
+    }
+    if (!supportedTypes(schema.attributes[item].type)) {
+      continue;
+    }
+    if (schema.attributes[item].type === "component") {
+      continue;
+    }
+    if (schema.attributes[item].type === "media") {
+      if (data.attributes[item].data === null) {
+        nullsNames.push(item);
+      }
+    }
     if (data.attributes[item] === null) {
       nullsNames.push(item);
     }
   }
   return nullsNames;
+}
+
+function supportedTypes(type: string): boolean {
+  let isSupported: boolean;
+  if (
+    type === "string" ||
+    type === "text" ||
+    type === "email" ||
+    type === "richtext" ||
+    type === "password" ||
+    type === "biginteger" ||
+    type === "decimal" ||
+    type === "float" ||
+    type === "enumeration" ||
+    type === "date" ||
+    type === "media" ||
+    type === "boolean" ||
+    type === "json" ||
+    type === "uid" ||
+    type === "enumeration"
+  ) {
+    isSupported = true;
+  } else isSupported = false;
+  return isSupported;
 }
 
 async function combineWithRelation(
@@ -119,7 +157,8 @@ async function combineWithRelation(
       dataToUpdate = compareFieldsAndUpdate(
         nullsNames,
         relationData,
-        dataToUpdate
+        dataToUpdate,
+        collectionUID
       );
       idsChecked.push(relationData.id);
 
@@ -146,10 +185,18 @@ async function getRelationData(
   relationsNames: string[],
   nullsNames: string[]
 ): Promise<Entity> {
+  let nullsArrayCopy = [...nullsNames];
+  let mediaNulls: string[] = [];
+  const schema: Schema = strapi.contentTypes[collectionUID].__schema__;
+  for (let i = nullsArrayCopy.length - 1; i >= 0; i--) {
+    if (schema.attributes[nullsArrayCopy[i]].type === "media") {
+      mediaNulls = [...nullsArrayCopy.splice(i, 1), ...mediaNulls];
+    }
+  }
   return await strapi.entityService.findOne(collectionUID, idToCheck, {
     fields: [],
     populate: relationsNames.reduce((acc, relation) => {
-      acc[relation] = { fields: nullsNames };
+      acc[relation] = { populate: mediaNulls, fields: nullsArrayCopy };
       return acc;
     }, {}),
   });
@@ -158,12 +205,42 @@ async function getRelationData(
 function compareFieldsAndUpdate(
   nullsNames: string[],
   relationData: { id: number; [key: string]: any },
-  dataToUpdate: Data
+  dataToUpdate: Data,
+  collectionUID: string
 ): Data {
   let tempArrayCopy = [...nullsNames];
   for (const item of nullsNames) {
     if (relationData[item] !== null) {
-      dataToUpdate.attributes[item] = relationData[item];
+      const schema: Schema = strapi.contentTypes[collectionUID].__schema__;
+      if (
+        schema.attributes[item].type === "media" &&
+        strapi.contentTypes[collectionUID].__schema__.attributes[item]
+          .multiple === false
+      ) {
+        let organizedData = {
+          id: relationData[item].id,
+          attributes: { ...relationData[item] },
+        };
+
+        delete organizedData.attributes.id;
+        dataToUpdate.attributes[item] = { data: organizedData };
+      } else if (
+        schema.attributes[item].type === "media" &&
+        strapi.contentTypes[collectionUID].__schema__.attributes[item]
+          .multiple === true
+      ) {
+        let organizedArray = relationData[item].map((mediaItem) => {
+          let data = {
+            id: mediaItem.id,
+            attributes: { ...mediaItem },
+          };
+          delete data.attributes.id;
+          return data;
+        });
+        dataToUpdate.attributes[item] = { data: organizedArray };
+      } else {
+        dataToUpdate.attributes[item] = relationData[item];
+      }
       tempArrayCopy.splice(tempArrayCopy.indexOf(item), 1);
     }
   }
